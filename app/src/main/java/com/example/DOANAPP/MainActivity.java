45bbd2f5851ff81ffcc0e4ead70ba3f1a9c76260
quangquyen2702 <quyentnq204112st.uel.edu.vn>
2022-11-14 10:04:36 +0700

    link

--- app/src/main/java/com/example/DOANAPP/MainActivity.java
+++ app/src/main/java/com/example/DOANAPP/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addEvent();
+        Link();
+    }
+
+    private void Link() {
     }
 
     private void addEvent() {

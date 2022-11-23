a1fea8bc8dd72e80bb9ee68a86f5975a47580205
PT <thaobp20411@st.uel.edu.vn>
2022-11-23 15:40:41 +0700

    commit5

---
+++ app/src/main/java/com/example/DOANAPP/confirm_OPT.java
@@ -0,0 +1,14 @@
+package com.example.DOANAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class confirm_OPT extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_opt);
+    }
+}
---
+++ app/src/main/java/com/example/DOANAPP/forgot_pwd.java
@@ -0,0 +1,14 @@
+package com.example.DOANAPP;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class forgot_pwd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pwd);
+    }
+}
